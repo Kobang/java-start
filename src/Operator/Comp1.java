47b1f3d77b713e1e0fb1dc61efce9ac6ffd42079
nimha82 <nimha82@naver.com>
2024-09-05 00:50:16 +0900

    ..

---
+++ src/Operator/Comp1.java
@@ -0,0 +1,14 @@
+package Operator;
+
+public class Comp1 {
+    public static void main(String[] args) {
+        int a = 3;
+        int b = 3;
+
+        System.out.println(a == b);
+        System.out.println(a != b);
+        System.out.println(a > b);
+        System.out.println(a >= b);
+        System.out.println(a <= b);
+    }
+}
